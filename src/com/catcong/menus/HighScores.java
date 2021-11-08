d730215c2acd14b5861be76b89dcae420c3c9fac
isabeldailey <isabeldailey@email.arizona.edu>
2021-11-07 16:48:08 -0800

    Home Screen And High Score Menus (Mostly Created)
    The Home Menu is fully created and I'm having trouble getting the table to show up in the High Scores menu, but the button layout is good.

--- src/com/catcong/menus/HighScores.java
+++ src/com/catcong/menus/HighScores.java
@@ -4,11 +4,12 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 
-public class HomeScreen extends JFrame{
+public class HighScores extends JFrame{
 private final int windowWidth = 800;
 private final int windowHeight = 800;
 
-	public HomeScreen() {
+
+	public HighScores() {
 		// Set title of window
 		super("Cat Cong");
 		
@@ -30,33 +31,33 @@ private final int windowHeight = 800;
 		getContentPane().setBackground(ArizonaBlue);
 	
 		// Create game title and borders
-		JLabel gameTitle = new JLabel("CAT CONG");
+		JLabel gameTitle = new JLabel("High Scores");
 		Border buttonBorder = BorderFactory.createLineBorder(UARed, 3);
 	
 		// Add and style game title label to pane
 		gameTitle.setLocation(250, 100);
 		gameTitle.setSize(450, 40);
 		gameTitle.setVerticalAlignment(JLabel.TOP);
-		gameTitle.setFont(new Font("Verdana", Font.BOLD, 38));
+		gameTitle.setFont(new Font("Verdana", Font.BOLD, 30));
 		gameTitle.setForeground(Color.WHITE);
 		gameTitle.setBackground(UARed);
 		getContentPane().add(gameTitle);
 		
 		// Add and style buttons and add them to pane
-		JButton highScore = new JButton("High Scores");
+		JButton homeScreen = new JButton("Home Screen");
 		JButton howToPlay = new JButton("How To Play");
 		JButton play = new JButton("Play");
 		JButton quit = new JButton("Quit");
 		
-		highScore.setLocation(274, 200);
-		highScore.setSize(150, 50);
-		highScore.setForeground(ArizonaBlue);
-		highScore.setBackground(Color.WHITE);
-		highScore.setHorizontalAlignment(JLabel.CENTER);
-		highScore.setBorder(buttonBorder);
-		getContentPane().add(highScore);
+		homeScreen.setLocation(24, 200);
+		homeScreen.setSize(150, 50);
+		homeScreen.setForeground(ArizonaBlue);
+		homeScreen.setBackground(Color.WHITE);
+		homeScreen.setHorizontalAlignment(JLabel.CENTER);
+		homeScreen.setBorder(buttonBorder);
+		getContentPane().add(homeScreen);
 		
-		howToPlay.setLocation(274, 300);
+		howToPlay.setLocation(224, 200);
 		howToPlay.setSize(150, 50);
 		howToPlay.setForeground(ArizonaBlue);
 		howToPlay.setBackground(Color.WHITE);
@@ -64,7 +65,7 @@ private final int windowHeight = 800;
 		howToPlay.setBorder(buttonBorder);
 		getContentPane().add(howToPlay);
 		
-		play.setLocation(274, 400);
+		play.setLocation(424, 200);
 		play.setSize(150, 50);
 		play.setForeground(ArizonaBlue);
 		play.setBackground(Color.WHITE);
@@ -72,20 +73,37 @@ private final int windowHeight = 800;
 		play.setBorder(buttonBorder);
 		getContentPane().add(play);
 		
-		quit.setLocation(274, 500);
+		quit.setLocation(624, 200);
 		quit.setSize(150, 50);
 		quit.setForeground(ArizonaBlue);
 		quit.setBackground(Color.WHITE);
 		quit.setHorizontalAlignment(JLabel.CENTER);
 		quit.setBorder(buttonBorder);
 		getContentPane().add(quit);
-	
+		
+		
+		/* Creating the high scores table */
+		// Data that's displayed in table
+		String[][] data = {{"Wilbur"}, {"10000"}, {"03-12-1885"}};
+		
+		// Column names
+		String[] columns = {"Name", "Score", "Date Achieved"};
+		
+		// Initialize the table
+		JTable highScores = new JTable(data, columns);
+		highScores.setBounds(30, 30, 30, 30);
+		
+		// Add it to the JScrollPane
+		JScrollPane sp = new JScrollPane(highScores);
+		getContentPane().add(sp);
+		
+		
 		// Display the window
 		setVisible(true);
 	
 	}
 
 	public static void main(String[] args) {
-		new HomeScreen();
+		new HighScores();
 	}
 }
